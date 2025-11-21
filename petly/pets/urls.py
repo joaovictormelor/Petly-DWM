6df0f6fe8e21808d1e731ee2d39b4f212138df7c
joao <joaovictormelodonascimento@gmail.com>
2025-11-20 22:10:30 -0300

    agora e possivel adicionar pets

--- petly/pets/urls.py
+++ petly/pets/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import listar_pets, meus_pets
+from .views import listar_pets, meus_pets, NovoPet
 
 urlpatterns = [
     path('', listar_pets, name='listar_pets'),
     path('meus/', meus_pets, name='meus_pets'),
+    path('novo/', NovoPet.as_view(), name='novo_pet'),
 ]
