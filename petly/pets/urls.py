f82087b77c237a7165fe6133378792d1b2e67c9a
joao <joaovictormelodonascimento@gmail.com>
2025-11-22 12:30:05 -0300

    edicao e exclusao de pets

--- petly/pets/urls.py
+++ petly/pets/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import listar_pets, meus_pets, NovoPet
+from .views import listar_pets, meus_pets, NovoPet, EditarPet, ExcluirPet
 
 urlpatterns = [
     path('', listar_pets, name='listar_pets'),
     path('meus/', meus_pets, name='meus_pets'),
     path('novo/', NovoPet.as_view(), name='novo_pet'),
+    path('editar/<int:pk>/', EditarPet.as_view(), name='editar_pet'),
+    path('excluir/<int:pk>/', ExcluirPet.as_view(), name='excluir_pet'),
 ]
+
