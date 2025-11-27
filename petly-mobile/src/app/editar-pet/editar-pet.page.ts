d46b2df8471b3ed9543a9b0aaec881fc90b02b18
joao <joaovictormelodonascimento@gmail.com>
2025-11-27 15:25:38 -0300

    correcocoes

--- petly-mobile/src/app/editar-pet/editar-pet.page.ts
+++ petly-mobile/src/app/editar-pet/editar-pet.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule, ToastController, LoadingController } from '@ionic/angular';
-import { ActivatedRoute, Router } from '@angular/router'; // Para pegar o ID e navegar
+import { ActivatedRoute, Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -14,7 +14,6 @@ import { HttpClient } from '@angular/common/http';
 })
 export class EditarPetPage implements OnInit {
 
-  // Objeto que guarda os dados (começa vazio)
   pet: any = {
     nome: '',
     especie: '',
@@ -35,10 +34,8 @@ export class EditarPetPage implements OnInit {
   ) { }
 
   ngOnInit() {
-    // 1. Pega o ID da URL
     this.petId = this.route.snapshot.paramMap.get('id');
     
-    // 2. Busca os dados atuais para preencher o formulário
     this.carregarDados();
   }
 
@@ -49,7 +46,7 @@ export class EditarPetPage implements OnInit {
     this.http.get(this.baseUrl + '/pets/api/gerenciar/' + this.petId + '/').subscribe({
       next: async (dados: any) => {
         await loader.dismiss();
-        this.pet = dados; // O formulário vai se preencher sozinho!
+        this.pet = dados;
       },
       error: async (erro) => {
         await loader.dismiss();
@@ -63,18 +60,14 @@ async salvarEdicao() {
     const loader = await this.loading.create({ message: 'Salvando...' });
     await loader.present();
 
-    // 1. Criamos uma cópia dos dados para não estragar a tela
     const dadosParaEnviar = { ...this.pet };
 
-    // 2. REMOVEMOS A FOTO E OS DADOS EXTRAS DO PACOTE
-    // Se mandarmos a URL da foto (texto) para o Django, ele trava.
     delete dadosParaEnviar.foto; 
     delete dadosParaEnviar.dono_email;
     delete dadosParaEnviar.dono_telefone;
     delete dadosParaEnviar.cidade;
     delete dadosParaEnviar.uf;
 
-    // 3. Mudamos de PUT para PATCH (Atualização Parcial)
     this.http.patch(this.baseUrl + '/pets/api/gerenciar/' + this.petId + '/', dadosParaEnviar).subscribe({
       next: async (res) => {
         await loader.dismiss();
@@ -84,7 +77,6 @@ async salvarEdicao() {
       error: async (erro) => {
         await loader.dismiss();
         console.error(erro);
-        // Dica: Imprime o erro detalhado no console para a gente ver se persistir
         console.log(JSON.stringify(erro.error)); 
         this.exibirMensagem('Erro ao atualizar.', 'danger');
       }
